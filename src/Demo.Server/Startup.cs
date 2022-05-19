2a5c0ab8e56f9b3d315b41c3388b3d51bfc2253d
q00Dree <shard.f@mail.ru>
2022-05-19 12:26:52 +0300

    [master] added entry point.

---
+++ src/Demo.Server/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder _)
+        {
+
+        }
+    }
+}

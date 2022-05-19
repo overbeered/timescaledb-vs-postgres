196decc72456f6719b8d84180c3fe23d72127a9b
Artur Safiullin <shard.f@mail.ru>
2022-05-19 13:50:06 +0300

    [master] added shared project for configuring model.

---
+++ src/Database/Contexts/Demo.Database.Contexts.Shared/Configuring/TimeEventDataConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Demo.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Demo.Database.Contexts.Shared
+{
+    internal class TimeEventDataConfiguration : IEntityTypeConfiguration<TimeEventData>
+    {
+        public void Configure(EntityTypeBuilder<TimeEventData> builder)
+        {
+            builder.ToTable("timeeventsdata");
+
+            builder.HasNoKey();
+
+            builder.HasIndex(timeEventData => timeEventData.EventId);
+
+            builder.HasIndex(timeEventData => new { timeEventData.Timestamp, timeEventData.StudentId });
+
+            builder.Property(timeEventData => timeEventData.StudentId)
+                .HasColumnName("studentid");
+
+            builder.Property(timeEventData => timeEventData.EventId)
+                .HasColumnName("eventid");
+
+            builder.Property(timeEventData => timeEventData.Timestamp)
+                .HasColumnName("timestamp")
+                .HasColumnType("timestamp with time zone");
+
+            builder.Property(timeEventData => timeEventData.Payload)
+                .HasColumnName("payload")
+                .HasColumnType("json")
+                .IsRequired();
+        }
+    }
+}
--- src/Database/Demo.Database.Models/TimeEventData.cs
+++ src/Database/Demo.Database.Models/TimeEventData.cs
@@ -7,5 +7,15 @@
         public Guid EventId { get; set; }
 
         public DateTimeOffset Timestamp { get; set; }
+
+        public string Payload { get; set; }
+
+        public TimeEventData(Guid studentId, Guid eventId, DateTimeOffset timestamp, string payload)
+        {
+            StudentId = studentId;
+            EventId = eventId;
+            Timestamp = timestamp;
+            Payload = payload;
+        }
     }
 }

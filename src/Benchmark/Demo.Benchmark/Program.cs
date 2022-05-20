134ce2442087f3817274d52d41f80d52b9467fab
gena56rus1 <gena56rus1@gihub.com>
2022-05-20 14:21:18 +0300

    [master] benchmark

---
+++ src/Benchmark/Demo.Benchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using Demo.Benchmark;
+
+BenchmarkRunner.Run<RepositoryBenchmark>();
---
+++ src/Benchmark/Demo.Benchmark/RepositoryBenchmark.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using Demo.Database.Contexts.Postgres;
+using Demo.Database.Contexts.TimescaleDB;
+using Demo.Database.Models;
+using Demo.Database.Repositories;
+
+namespace Demo.Benchmark
+{
+    internal class RepositoryBenchmark
+    {
+        private readonly PostgresDbContext _contextPostgres;
+        private readonly TimescaleDbContext _contextTimescale;
+        private readonly TimeEventData _timeEventData;
+        private readonly List<TimeEventData> _listTimeEventData1000;
+
+        public RepositoryBenchmark(PostgresDbContext contextPostgres, TimescaleDbContext contextTimescale)
+        {
+            _contextPostgres = contextPostgres;
+            _contextTimescale = contextTimescale;
+            _timeEventData = new TimeEventData(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.UtcNow, "test");
+            _listTimeEventData1000 = new List<TimeEventData>();
+            for (int i = 0; i < 999; i++)
+            {
+                _listTimeEventData1000.Add(_timeEventData);
+            }
+        }
+
+        [Benchmark]
+        public void TimescaleDbTimeEventData()
+        {
+            var repository = new TimescaleDbTimeEventDataRepository(_contextTimescale);
+            repository.AddTimeEventDataAsync(_timeEventData);
+        }
+
+        [Benchmark]
+        public void PostgresDbTimeEventData()
+        {
+            var repository = new PostgresDbTimeEventDataRepository(_contextPostgres);
+            repository.AddTimeEventDataAsync(_timeEventData);
+        }
+
+        [Benchmark]
+        public void TimescaleDbTimeEventData1000()
+        {
+            var repository = new TimescaleDbTimeEventDataRepository(_contextTimescale);
+            repository.AddTimeEventsDataAsync(_listTimeEventData1000);
+        }
+
+        [Benchmark]
+        public void PostgresDbTimeEventData1000()
+        {
+            var repository = new PostgresDbTimeEventDataRepository(_contextPostgres);
+            repository.AddTimeEventsDataAsync(_listTimeEventData1000);
+        }
+    }
+}

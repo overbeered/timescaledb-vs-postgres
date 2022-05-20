4b6cc872a70a9604d5e9242c2e858adf2cf92885
Artur Safiullin <shard.f@mail.ru>
2022-05-20 16:02:47 +0300

    [master] created.

--- src/Benchmark/Demo.Benchmark/RepositoryBenchmark.cs
+++ src/Benchmark/Demo.Benchmark/RepositoryBenchmark.cs
@@ -3,54 +3,70 @@ using Demo.Database.Contexts.Postgres;
 using Demo.Database.Contexts.TimescaleDB;
 using Demo.Database.Models;
 using Demo.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace Demo.Benchmark
 {
-    internal class RepositoryBenchmark
+    public class RepositoryBenchmark
     {
-        private readonly PostgresDbContext _contextPostgres;
         private readonly TimescaleDbContext _contextTimescale;
+        private readonly PostgresDbContext _contextPostgres;
+
         private readonly TimeEventData _timeEventData;
+
         private readonly List<TimeEventData> _listTimeEventData1000;
 
-        public RepositoryBenchmark(PostgresDbContext contextPostgres, TimescaleDbContext contextTimescale)
+        public RepositoryBenchmark()
         {
-            _contextPostgres = contextPostgres;
-            _contextTimescale = contextTimescale;
-            _timeEventData = new TimeEventData(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.UtcNow, "test");
+            var timescaleDbContext = new DbContextOptionsBuilder<TimescaleDbContext>()
+                .UseNpgsql("Host=localhost;Port=5002;Database=timescaledb-ted-database;Username=postgres;Password=postgres")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .Options;
+
+            _contextTimescale = new TimescaleDbContext(timescaleDbContext);
+
+            var postgresDbContext = new DbContextOptionsBuilder<PostgresDbContext>()
+                .UseNpgsql("Host=localhost;Port=5001;Database=postgres-ted-database;Username=postgres;Password=postgres")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .Options;
+
+            _contextPostgres = new PostgresDbContext(postgresDbContext);
+
+            _timeEventData = new TimeEventData(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.UtcNow, JsonSerializer.Serialize(new { DataId = "AYE" }));
             _listTimeEventData1000 = new List<TimeEventData>();
-            for (int i = 0; i < 999; i++)
+            for (int i = 1; i <= 1000; i++)
             {
                 _listTimeEventData1000.Add(_timeEventData);
             }
         }
 
-        [Benchmark]
-        public void TimescaleDbTimeEventData()
-        {
-            var repository = new TimescaleDbTimeEventDataRepository(_contextTimescale);
-            repository.AddTimeEventDataAsync(_timeEventData);
-        }
+        //[Benchmark]
+        //public void TimescaleDbTimeEventData()
+        //{
+        //    var repository = new TimescaleDbTimeEventDataRepository(_contextTimescale);
+        //    repository.AddTimeEventDataAsync(_timeEventData);
+        //}
 
-        [Benchmark]
-        public void PostgresDbTimeEventData()
-        {
-            var repository = new PostgresDbTimeEventDataRepository(_contextPostgres);
-            repository.AddTimeEventDataAsync(_timeEventData);
-        }
+        //[Benchmark]
+        //public void PostgresDbTimeEventData()
+        //{
+        //    var repository = new PostgresDbTimeEventDataRepository(_contextPostgres);
+        //    repository.AddTimeEventDataAsync(_timeEventData);
+        //}
 
-        [Benchmark]
-        public void TimescaleDbTimeEventData1000()
-        {
-            var repository = new TimescaleDbTimeEventDataRepository(_contextTimescale);
-            repository.AddTimeEventsDataAsync(_listTimeEventData1000);
-        }
+        //[Benchmark]
+        //public void TimescaleDbTimeEventData1000()
+        //{
+        //    var repository = new TimescaleDbTimeEventDataRepository(_contextTimescale);
+        //    repository.AddTimeEventsDataAsync(_listTimeEventData1000);
+        //}
 
-        [Benchmark]
-        public void PostgresDbTimeEventData1000()
-        {
-            var repository = new PostgresDbTimeEventDataRepository(_contextPostgres);
-            repository.AddTimeEventsDataAsync(_listTimeEventData1000);
-        }
+        //[Benchmark]
+        //public void PostgresDbTimeEventData1000()
+        //{
+        //    var repository = new PostgresDbTimeEventDataRepository(_contextPostgres);
+        //    repository.AddTimeEventsDataAsync(_listTimeEventData1000);
+        //}
     }
 }
---
+++ src/Demo.Playground/Program.cs
@@ -0,0 +1,79 @@
+﻿using Demo.Database.Contexts.Postgres;
+using Demo.Database.Contexts.TimescaleDB;
+using Demo.Database.Models;
+using Demo.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace Demo.Playground
+{
+#nullable disable
+    public class Program
+    {
+        private static TimeEventData _timeEventData;
+        private static List<TimeEventData> _listTimeEventData;
+
+        private static TimescaleDbTimeEventDataRepository _timescaleDbRepository;
+        private static PostgresDbTimeEventDataRepository _postgresDbRepository;
+
+        //public static void Main(string[] args)
+        //{
+
+        //}
+
+        public static async Task Main(string[] args)
+        {
+            // 65535
+            // 4
+
+            Init(16383);
+
+            int total = await TestPostgresDbAsync();
+            Console.WriteLine(total);
+        }
+
+        public static async Task<int> TestPostgresDbAsync()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            await _postgresDbRepository.AddTimeEventsDataAsync(_listTimeEventData);
+
+            timer.Stop();
+
+            TimeSpan timeTaken = timer.Elapsed;
+
+            Console.WriteLine("Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
+
+            return timeTaken.Seconds;
+        }
+
+        public static void Init(int itemsToCreate)
+        {
+            var timescaleDbContext = new DbContextOptionsBuilder<TimescaleDbContext>()
+                .UseNpgsql("Host=localhost;Port=5002;Database=timescaledb-ted-database;Username=postgres;Password=postgres")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .Options;
+            var contextTimescale = new TimescaleDbContext(timescaleDbContext);
+            _timescaleDbRepository = new TimescaleDbTimeEventDataRepository(contextTimescale);
+
+            var postgresDbContext = new DbContextOptionsBuilder<PostgresDbContext>()
+                .UseNpgsql("Host=localhost;Port=5001;Database=postgres-ted-database;Username=postgres;Password=postgres")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .Options;
+            var contextPostgres = new PostgresDbContext(postgresDbContext);
+            _postgresDbRepository = new PostgresDbTimeEventDataRepository(contextPostgres);
+
+            _timeEventData = new TimeEventData(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.UtcNow, JsonSerializer.Serialize(new { DataId = "AYE" }));
+
+            _listTimeEventData = new List<TimeEventData>();
+
+            for (int i = 0; i < itemsToCreate; i++)
+            {
+                _listTimeEventData.Add(_timeEventData);
+            }
+        }
+    }
+#nullable restore
+}

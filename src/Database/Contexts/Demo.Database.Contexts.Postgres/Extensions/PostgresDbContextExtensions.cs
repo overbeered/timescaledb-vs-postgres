0eb2d85591e2aa73c0de7eb79c5861d865514573
Artur Safiullin <shard.f@mail.ru>
2022-05-21 14:48:51 +0300

    [master] added cleanup code for both contexts.

---
+++ src/Database/Contexts/Demo.Database.Contexts.Postgres/Extensions/PostgresDbContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Database.Contexts.Postgres.Extensions
+{
+    public static class PostgresDbContextExtensions
+    {
+        public static Task TruncateTableAsync(this PostgresDbContext context,
+            string hypertableName)
+        {
+            string query = $"TRUNCATE TABLE {hypertableName}";
+
+            return context.Database.ExecuteSqlRawAsync(query);
+        }
+    }
+}
---
+++ src/Database/Contexts/Demo.Database.Contexts.TimescaleDB/Extensions/TimescaleDbContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace Demo.Database.Contexts.TimescaleDB.Extensions
+{
+    public static class TimescaleDbContextExtensions
+    {
+        public static Task DropChunksNewerThanAsync(this TimescaleDbContext context,
+            string hypertableName,
+            DateTimeOffset startPoint)
+        {
+            string query = $"SELECT drop_chunks('{hypertableName}', newer_than => @startPoint);";
+
+            var parameters = new List<NpgsqlParameter>
+            {
+                new NpgsqlParameter("@startPoint", NpgsqlTypes.NpgsqlDbType.TimestampTz) { Value = startPoint }
+            };
+
+            return context.Database.ExecuteSqlRawAsync(query, parameters);
+        }
+
+        public static Task DropChunksNewerThanNegativeInfinityAsync(this TimescaleDbContext context,
+                    string hypertableName)
+        {
+            return DropChunksNewerThanAsync(context, hypertableName, DateTimeOffset.MinValue);
+        }
+    }
+}
